72de31534e571dc044191e376fa0e62b1f9260bf
diegoamse <diegoamorinsr@gmail.com>
2018-12-12 22:03:49 -0500

    Agregamos los modelos Event y Game, las registramos en el admin y hacermos las migraciones

--- gameEvents/admin.py
+++ gameEvents/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event, Game
+
+admin.site.register([Event, Game])
--- gameEvents/models.py
+++ gameEvents/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	start_day = models.DateField()
+	end_day = models.DateField()
+
+	def __str__(self):
+		return self.name
+
+class Game(models.Model):
+	name = models.CharField(max_length=100)
+	day = models.DateTimeField()
+	events = models.ForeignKey(
+		Event,
+		related_name="games",
+		on_delete=models.CASCADE,
+		blank=True,
+		null=True,
+	)
+
+	def __str__(self):
+		return self.name
