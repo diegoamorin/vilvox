484ec3856c9b702334dcc7e344cd5a11955d3b2d
diegoamse <diegoamorinsr@gmail.com>
2018-12-14 16:55:34 -0500

    Agregamos el campo state y slug en los forms del eventForm

--- gameEvents/forms.py
+++ gameEvents/forms.py
@@ -5,9 +5,22 @@ from .models import Event, Game
 class EventForm(forms.ModelForm):
 	class Meta:
 		model = Event
-		fields = ['name', 'description', 'start_day', 'end_day', 'img']
+		fields = [
+			'name', 
+			'slug', 
+			'description', 
+			'start_day', 
+			'end_day', 
+			'img',
+		]
 
 class GameForm(forms.ModelForm):
 	class Meta:
 		model = Game
-		fields = ['name', 'day', 'events']
+		fields = [
+			'name', 
+			'slug', 
+			'day', 
+			'state', 
+			'events',
+		]
