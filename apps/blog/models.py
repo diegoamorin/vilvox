6fd97ce082b904de1138a79593524e70757cd6c4
diegoamse <diegoamorinsr@gmail.com>
2018-12-15 08:34:46 -0500

    (db) Volvemos unique el subject de los tags

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Tag(models.Model):
-	subject = models.CharField(max_length=20)
+	subject = models.CharField(max_length=20, unique=True)
 
 	def __str__(self):
 		return self.subject
