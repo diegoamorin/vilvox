75a244001ad7079488ab68e0974cd620d854541a
Diego Amorin <diegoamorinsr@gmail.com>
2019-01-02 19:08:23 -0500

    (db) cambios sobre el update field

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -21,8 +21,8 @@ class Post(models.Model):
 	title = models.CharField(max_length=100)
 	slug = models.SlugField(max_length=120, unique=True)
 	content = models.TextField()
-	created_at = models.DateTimeField(auto_now=True)
-	updated_at = models.DateTimeField(auto_now_add=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(null=True)
 	img = models.ImageField(upload_to='images/post/')
 	series = models.ManyToManyField(
 		List,
