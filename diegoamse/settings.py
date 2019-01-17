2c6bd06ac3ac978aa8b84a7db6a28530a3436aa9
Diego Amorin <diegoamorinsr@gmail.com>
2019-01-17 15:05:44 -0500

    cambiamos a usuario postgres

--- diegoamse/settings.py
+++ diegoamse/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'blog',
-        'USER': 'diegoamse',
+        'USER': 'postgres',
         'PASSWORD': '<%dieg6o>V',
         'HOST': 'localhost',
         'PORT': '',
