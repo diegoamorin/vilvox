d835f587eb3eef09ad72384eba2734f8739afbf1
Diego Amorin <diegoamorinsr@gmail.com>
2019-03-08 17:10:39 -0500

    Seccionamos los juegos por dias y las traducimos al castellano - con la biblioteca agregada utils convertimos al castellado y formateamos cadenas datetime - agregamos una logica no documentada para lograrlo

--- apps/events/views.py
+++ apps/events/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Event, Game
 from .forms import EventForm, GameForm
+from ..utils.date_spanish import get_month, get_weekday
+
+import pprint
 
 def eventIndex(request):
 	events = Event.objects.all()
@@ -19,15 +22,54 @@ def eventDetail(request, slug):
 		.get_queryset()
 		.order_by('day')
 	)
-	lista = [list(game.teams.get_queryset()) for game in games]
-	lista2 = [game.day for game in games]
-	lista3 = [game.pk for game in games]
-	lista_total = [
-		{'teams':i[0], 'day':i[1], 'pk':i[2]} for i in zip(lista, lista2, lista3)
+	gTeams = [list(game.teams.get_queryset()) for game in games]
+	gDay = [game.day for game in games]
+	gPk = [game.pk for game in games]
+	total_games = [
+		{'teams':i[0], 'day':i[1], 'pk':i[2]} for i in zip(gTeams, gDay, gPk)
 	]
+
+	days_format = []
+	for game in total_games:
+		day = game.get("day")
+		# day_format = Sábado 25 Mayo
+		days_format.append(
+			get_weekday(day.strftime("%A")) + " " + str(day.day) + " " + get_month(day.month)
+		)
+
+	print(days_format)
+
+	days_formats_and_games = [
+		{"format_day":i[0], "game":i[1]} for i in zip(days_format, total_games)
+	]
+
+	days_format_only = []
+	for day_format in days_format:
+		if not day_format in days_format_only:
+			days_format_only.append(day_format)
+
+	print(days_format_only)
+	bucle = []
+	for day_format in days_format_only:
+
+		games_list = []
+
+		for day_format_and_game in days_formats_and_games:
+			if day_format == day_format_and_game.get("format_day"):
+
+				games_list.append(day_format_and_game.get("game"))
+
+		result = {"format_day": day_format, "games": games_list}
+
+		bucle.append(result)
+
+	print("")
+	pprint.pprint(bucle)
+	print("")
+
 	return render(request, "detailEvent.html", {
 		"event": event,
-		"games": lista_total,
+		"games": total_games,
 	})
 
 @login_required
