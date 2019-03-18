5f87dd2ac952547beed23fc5e43a584fb2ed7ab4
Diego Amorin <diegoamorinsr@gmail.com>
2019-03-18 16:00:46 -0500

    Quitamos el campo fullname en el Profile model por que el User(DjangoModel) ya viene con first_name y last_name

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -26,7 +26,6 @@ class socialURL(models.Model):
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	fullname = models.CharField(max_length=40, blank=True, null=True)
 	img_perfil = models.ImageField('images/profiles/')
 	bio = models.TextField(max_length=500, blank=True, null=True)
 	location = models.CharField(max_length=30, blank=True)
